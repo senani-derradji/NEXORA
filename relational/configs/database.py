d05600aad0bc6b975828b99e5b1e5d3587989f55
senani-derradji <derradji.senani.1@gmail.com>
2026-01-25 13:59:19 +0100

    modified database and added readme file

--- relational/configs/database.py
+++ relational/configs/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # DEVELOPMENT
-DATABASE_URL = os.getenv("DEV_DATABASE_URL") or "sqlite:///test.db"
+DATABASE_URL = os.getenv("DATABASE_URL") or "sqlite:///test.db"
 engine = create_engine(DATABASE_URL, echo=True)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 base = declarative_base()
