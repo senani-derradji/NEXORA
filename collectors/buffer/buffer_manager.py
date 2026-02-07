cdd927e9bc5cb9e162169ec8f762f485be7bdced
senani-derradji <derradji.senani.1@gmail.com>
2026-02-07 14:47:33 +0100

    added buffer to the game xd

---
+++ collectors/buffer/buffer_manager.py
@@ -0,0 +1,32 @@
+from collectors.buffer.memory_queue import MemoryQueue
+from collectors.buffer.dbs_buffer.disk_queue import DatabaseBuffer
+from collectors.transport.grpc_client import CoreClient
+
+
+class BufferManager:
+    def __init__(self):
+        self.mem = MemoryQueue()
+        self.disk = DatabaseBuffer()
+        self.client = CoreClient()
+
+    def push_data(self, metric, status):
+
+        if status:
+            self.mem.push(metric)
+            disk_rows = self.disk.fetch_all_unsent()
+
+            if disk_rows:
+
+                for d in disk_rows:
+                    all_sent = True
+                    resp = self.client.send_metric(metric=d.payload)
+
+                    if not resp: all_sent = False ; break
+
+                    if all_sent: self.disk.mark_sent(d.id)
+
+                    else: break
+
+        else: self.disk.store_buffer(data=metric)
+
+    def pop_data(self): return self.mem.pop()
---
+++ collectors/buffer/dbs_buffer/disk_queue.py
@@ -0,0 +1,59 @@
+from sqlalchemy import create_engine, Select, inspect
+from sqlalchemy.orm import sessionmaker
+from collectors.buffer.dbs_buffer.database_model import SqlMetrics, Base
+
+
+class DatabaseBuffer:
+
+    def __init__(self, db_url="sqlite:///buffer.db"):
+        self.engine = create_engine(db_url)
+        self.inspector = inspect(self.engine)
+        self.tables = self.inspector.get_table_names()
+
+        if "buffer_metrics" not in self.tables: Base.metadata.create_all(bind=self.engine)
+
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+
+    def store_buffer(self, data: dict):
+
+        b_m = SqlMetrics(
+            device=data["device"].get("hostname", "unknown"),
+            metric_type=data["device"].get("type", "unknown"),
+            ots=data["timestamp"],
+            payload=data,
+            sent=False
+        )
+        self.session.add(b_m); self.session.commit(); self.session.close()
+        return True
+
+    def fetch_unsent(self, limit=100):
+        session = self.Session()
+        rows = session.execute(Select(SqlMetrics).where(SqlMetrics.sent == False).limit(limit)).scalars().all() ; session.close()
+        if not rows: return False
+        return rows
+
+
+    def fetch_all_unsent(self):
+        session = self.Session()
+        rows = session.execute(
+            Select(SqlMetrics).where(SqlMetrics.sent == False)
+        ).scalars().all()
+        session.close()
+        if not rows: return False
+        return rows
+
+
+    def mark_sent(self, id_):
+        session = self.Session()
+        try:
+            row = session.get(SqlMetrics, id_)
+            if not row:
+                return False
+
+            row.sent = 1
+            session.commit()
+            return True
+
+        finally:
+            session.close()
